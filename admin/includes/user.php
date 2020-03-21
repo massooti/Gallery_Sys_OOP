2d1a651eebf8792a84c8a279724e006d549e4a68
Amir11009 <Amirk30116@gmail.com>
2020-03-21 13:45:12 +0430

    start create method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -64,6 +64,16 @@ class  User{
       $object_properties = get_object_vars($this);
       return array_key_exists($the_attribute ,$object_properties );
     }
+
+    public function create(){
+      global $database;
+      $sql="INSERT INTO users (username,password,first_name,last_name)";
+      $sql .="VALUES ('";
+      $sql .=$database->escape_string($this->username)."','";
+      $sql .=$database->escape_string($this->password)."','";
+      $sql .=$database->escape_string($this->first_name)."','";
+      $sql .=$database->escape_string($this->last_name)."')";
+    }
 }
 
 //010 Logion creation download
