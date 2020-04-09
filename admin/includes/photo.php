afcdf5c0983673d23fec16617c8150c57c1880c4
Amir11009 <Amirk30116@gmail.com>
2020-04-09 20:51:04 +0430

    Photo Class properties

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,7 +1,14 @@
 <?php
 
 class Photo extends Db_object{
-
+    protected static $db_table="photos";
+    protected static $db_table_fields=array('photo_id','title','description','filename','size','type');
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $size;
+    public $type;
 
 
 
