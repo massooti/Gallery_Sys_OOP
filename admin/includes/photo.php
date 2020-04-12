59078e185884c1d10b7d991fc7c3fe113df49be8
Amir11009 <Amirk30116@gmail.com>
2020-04-12 16:48:25 +0430

    method save for Photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,8 +1,9 @@
 <?php
 
-class Photo extends Db_object{
-    protected static $db_table="photos";
-    protected static $db_table_fields=array('photo_id','title','description','filename','size','type');
+class Photo extends Db_object
+{
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array('photo_id', 'title', 'description', 'filename', 'size', 'type');
     public $photo_id;
     public $title;
     public $description;
@@ -14,17 +15,49 @@ class Photo extends Db_object{
     public $upload_directory = "images";
     public $custom_errors = array();
     public $upload_errors_array = array(
-        UPLOAD_ERR_OK        => "There is no error",
-        UPLOAD_ERR_INI_SIZE     => "The uploaded file exceeds the upload_max_filesize_directory",
-        UPLOAD_ERR_FORM_SIZE =>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize_directory",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
         UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
         UPLOAD_ERR_NO_FILE => "No file was uploaded",
-        UPLOAD_ERR_NO_TMP_DIR =>"Missing a temporary folder. Introduced in PHP 5.0.3",
-        UPLOAD_ERR_CANT_WRITE =>"Failed to write file to disk. Introduced in PHP 5.1.0",
-        UPLOAD_ERR_EXTENSION =>"A PHP extension stopped the file upload",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder. Introduced in PHP 5.0.3",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk. Introduced in PHP 5.1.0",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload",
     );
 
 
+    //This is passing $_file['uploaded_file'] as an argument
+    public function set_file($file)
+    {
+        if (empty($file) || !$file || !is_array($file)) {
+            $this->errors[] = "There was no file uploaded here";
+            return false;
+        } elseif ($file['error'] != 0) {
+            $this->errors[] = $this->upload_errors_array[$file['error']];
+            return false;
+        } else {
+            $this->filename = basename($file['name']);
+            $this->tmp_path = $file['temp_name'];
+            $this->type = $file['type'];
+            $this->size = $file['size'];
+        }
+    }
+
+
+    public function save(){
+        if($this->photo_id){
+            $this->update();
+        }else{
+            if(!empty($this->errors)){
+                return false;
+            }
+        }
+        if(empty($this->filename) || empty($this->tmp_path)){
+            $this->errors[] = "The file was not available";
+            return false;
+        }
+        $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+    }
 }
 
 ?>
