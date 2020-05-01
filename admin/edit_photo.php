e48fdba517d4568963d1fd5fa3f30a5036c03cd2
Amir11009 <Amirk30116@gmail.com>
2020-05-01 15:04:10 +0430

    add details - bottuns in edit photo page

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -7,7 +7,12 @@
 <?php include "includes/sidebar.php" ?>
 <!-- /.navbar-collapse -->
 </nav>
-<?php   $photos =Photo::find_all() ?>
+<?php
+if(isset($_POST['update'])){
+
+    echo "it works";
+}
+?>
 <div id="page-wrapper">
     <div class="container-fluid">
 
@@ -27,26 +32,62 @@
                     </li>
                 </ol>
             </div>
+            <form action="" method="post">
+                <div class="col-md-8">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="Caption">Caption</label>
+                        <input type="text" name="caption" class="form-control">
+                    </div>
 
-            <div class="col-md-12">
-                <div class="form-group">
-                    <input type="text" name="title" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="Caption">Caption</label>
-                    <input type="text" name="caption" class="form-control">
+                    <div class="form-group">
+                        <label for="Caption">Alternate Text</label>
+                        <input type="text" name="alternate_text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Caption">Description</label>
+                        <textarea type="text" name="description" class="form-control" id="" rows="38" col="10"></textarea>
+                    </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="Caption">Alternate Text</label>
-                    <input type="text" name="alternate_text" class="form-control">
-                </div>
+                <div class="col-md-4">
+                    <div class="photo-info-box">
+                        <h4>save<span id="toggle" class="glyphicon glyphicon-menu-up"></span></h4>
+                    </div>
+                    <div class="inside">
+                        <div class="box-inner">
+                            <p class="text">
+                                <span class="glyphicon glyphicon-calendar"></span>Uploaded on:
+                            </p>
+                            <p class="text">
+                                Photo Id: <span class="data photo_id_box">34</span>
+                            </p>
+                            <p class="text">
+                                Filename: <span class="data">image.jpg</span>
+                            </p>
+                            <p class="text">
+                                File Type: <span class="data">JPG</span>
+                            </p>
+                            <p class="text">
+                                File Size: <span class="data">34444</span>
+                            </p>
+                        </div>
+                        <div class="info-box-footer clearfix">
+                            <div class="info-box-delete pull-left">
+                                <a href="delete_photo.php?=id<?php echo $photo->id;?>"></a>
+                            </div>
+                            <div class="info-box-update pull-right">
+                                <input type="submit" name="update" value="Update">
+                            </div>
+                        </div>
 
-                <div class="form-group">
-                    <label for="Caption">Description</label>
-                    <textarea type="text" name="description" class="form-control" id="" rows="38" col="10"></textarea>
+                    </div>
                 </div>
-            </div>
+            </form>
+
         </div>
         <!-- /.row -->
 
